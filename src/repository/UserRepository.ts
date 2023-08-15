4d8166a283143e1d45980350756a67f407ea71a7
tuNovio <alfgomachi@gmail.com>
2023-08-15 14:39:16 -0500

    verificación de la base de datos y la tabla de datos

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -7,17 +7,17 @@ export class UserRepository implements DbQueryModel {
     constructor(){
         this.query = new DbConection().normal_connection;
     }
-    verificate_database(db_name: string): Promise<ResultSetHeader | undefined> {
+    verificate_database(): Promise<ResultSetHeader | undefined> {
         return new Promise((resolve, reject)=>{
-            this.query.execute(`create database if not exists ${db_name}`, function(err: QueryError | null, res: ResultSetHeader | undefined){
+            this.query.execute('create database if not exists consulta', function(err: QueryError | null, res: ResultSetHeader | undefined){
                 if(err)reject(err)
                 resolve(res)
             })
         })
     }
-    select_database(db_name: string): Promise<mysql.ResultSetHeader | undefined> {
+    select_database(): Promise<mysql.ResultSetHeader | undefined> {
         return new Promise((resolve, reject)=>{
-            this.query.query(`use ${db_name}`, function(err: QueryError | null, res: ResultSetHeader | undefined){
+            this.query.query('use consulta', function(err: QueryError | null, res: ResultSetHeader | undefined){
                 if(err)reject(err)
                 resolve(res)
             })
