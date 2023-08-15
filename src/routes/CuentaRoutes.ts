78c1eb022c29240755d24337509759a7d7a59cf7
tuNovio <alfgomachi@gmail.com>
2023-08-15 18:41:54 -0500

    se agrega la autenticación de las cookies para las cuentas

--- src/routes/CuentaRoutes.ts
+++ src/routes/CuentaRoutes.ts
@@ -1,15 +1,20 @@
 import { RoutesModel } from "../Models/RoutesModel";
 import {CuentaController} from "../controllers/CuentaController"
+import {AuthMiddleware} from "../middlewares/AuthMiddleware"
 import express from "express"
 
 export class CuentaRoutes implements RoutesModel{
     private cuenta_router: express.Router
     private controller: CuentaController
+    private autenticate_user: AuthMiddleware;
     constructor(){
         this.cuenta_router = express.Router()
         this.controller = new CuentaController();
+        this.autenticate_user = new AuthMiddleware();
     }
     UseMiddleware(){
+        //verificate the user login
+        this.cuenta_router.use( this.autenticate_user.verificar_autenticacion.bind(this.autenticate_user))
         this.cuenta_router.use(this.controller.verificate_data_cuenta.bind(this.controller))
     }
     get_all_route(): void {
