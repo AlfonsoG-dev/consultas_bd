3ef3b2fd90bcd2aad61458de7566a7a737c4a4be
tuNovio <alfgomachi@gmail.com>
2023-08-15 14:42:06 -0500

    actualizat las dependencias de los métodos

--- src/controllers/CuentaController.ts
+++ src/controllers/CuentaController.ts
@@ -16,7 +16,7 @@ export class CuentaController{
 
     async verificate_data_cuenta(req: Request, res: Response, next: NextFunction){
         try{
-            const data_res = Promise.all([this.repository.verificate_database('consulta'), this.repository.select_database('consulta'), this.repository.verificate_table()])
+            const data_res = Promise.all([this.repository.verificate_database(), this.repository.select_database(), this.repository.verificate_table()])
             console.log(await data_res)
             next()
         }catch(err: CuentaControllerError){
