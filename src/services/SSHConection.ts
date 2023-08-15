9e8455f7fb3d530290d1a25b791acb397546ea98
tuNovio <alfgomachi@gmail.com>
2023-08-15 17:24:45 -0500

    conexión por ssh a la base de datos

--- src/services/SSHConection.ts
+++ src/services/SSHConection.ts
@@ -1,34 +1,51 @@
 import {Client} from"ssh2"
-import { ServerApiEnum } from "../Models/ServerEnums"
+import mysql, {Connection, QueryError} from "mysql2"
 
 export class SSHConection{
     private client: Client;
-    private host: Readonly<string>
-    private port: Readonly<number>
-    private username: Readonly<string>
-    private password: Readonly<string>
     constructor(){
         this.client = new Client()
-        this.host = ServerApiEnum.HOST_SSH
-        this.port = ServerApiEnum.SSH_PORT 
-        this.username = ServerApiEnum.USER_SSH
-        this.password = ServerApiEnum.PASSWORD_USER_SSH
     }
-    get tunel_config(){
-        return {
-            host: this.host,
-            port: this.port,
-            username: this.username,
-            password: this.password
+    ssh_conection(){
+        const dbServer = {
+            host: 'localhost',
+            port: 3306,
+            user: 'pym',
+            password: '5x5W12',
+            database: 'consulta'
         }
-    }
-    ssh_conection(dstIP: string, dstPort: number){
-        return new Promise((resolve, reject) =>{
+
+        const tunel = {
+            host: '192.168.2.114',
+            port: 22,
+            username: 'server',
+            password: '{alf}'
+        }
+        const forward = {
+            srcHost: '127.0.0.1',
+            srcPort: 3306,
+            dstHost: dbServer.host,
+            dstPort: dbServer.port
+        }
+        const ssh_con: Promise<Connection> =  new Promise((resolve, reject)=>{
             this.client.on('ready', () =>{
-                this.client.forwardOut(this.tunel_config.host, this.tunel_config.port, dstIP, dstPort, (err, stream)=>{
+                this.client.forwardOut(forward.srcHost, forward.dstPort, forward.dstHost, forward.dstPort, (err, stream)=>{
                     if(err)reject(err)
+                    const update_db_server ={
+                        ...dbServer,
+                        stream
+                    }
+
+                    const conection: Connection = mysql.createConnection(update_db_server)
+                    conection.connect((err: QueryError | null) =>{
+                        if(err){
+                            reject(err)
+                        }
+                        resolve(conection)
+                    })
                 })
-            })
+            }).connect(tunel)
         })
+        return ssh_con
     }
 }
