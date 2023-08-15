12aa346f9bca456fde6e24d80bbcc12c6879fa64
tuNovio <alfgomachi@gmail.com>
2023-08-15 17:41:57 -0500

    se crea la conexión por ssh a mysql-server

--- src/services/SSHConection.ts
+++ src/services/SSHConection.ts
@@ -1,39 +1,44 @@
 import {Client} from"ssh2"
 import mysql, {Connection, QueryError} from "mysql2"
-import {ConsoleConstructor} from "console";
+import { DbConfig } from "../Models/DBConfig"
 
 export class SSHConection{
     private client: Client;
+    private dbServer: DbConfig;
     constructor(){
         this.client = new Client()
+        this.dbServer = new DbConfig()
     }
-    ssh_conection():Promise<Connection>{
-        const dbServer = {
-            host: 'localhost',
-            port: 3306,
-            user: 'pym',
-            password: '5x5W12',
-            database: 'consulta'
-        }
 
-        const tunel = {
+    get tunel_config(){
+        return {
             host: '192.168.2.114',
             port: 22,
             username: 'server',
             password: '{alf}'
         }
-        const forward = {
+    }
+
+    forward_config(dbServer: any){
+        return {
             srcHost: '127.0.0.1',
             srcPort: 3306,
             dstHost: dbServer.host,
             dstPort: dbServer.port
+
         }
+    }
+    ssh_conection():Promise<Connection>{
+        const db_server = this.dbServer.db_server_config
+        const tunel = this.tunel_config
+        const forward = this.forward_config(db_server)
+
         return new Promise((resolve, reject)=>{
             this.client.on('ready', () =>{
                 this.client.forwardOut(forward.srcHost, forward.dstPort, forward.dstHost, forward.dstPort, (err, stream)=>{
                     if(err)reject(err)
                     const update_db_server ={
-                        ...dbServer,
+                        ...db_server,
                         stream
                     }
 
