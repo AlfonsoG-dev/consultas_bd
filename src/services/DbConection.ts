e37d4c91ef9d223427e19bcc2b88b3452d2812bc
tuNovio <alfgomachi@gmail.com>
2023-08-15 17:40:53 -0500

    se provee de una conexión por ssh

--- src/services/DbConection.ts
+++ src/services/DbConection.ts
@@ -1,10 +1,13 @@
 import mysql, { Connection, Pool } from "mysql2"
 import { DbConfig } from "../Models/DBConfig";
+import { SSHConection } from "./SSHConection";
 export class DbConection {
-    private conection;
+    private conection: typeof mysql;
+    private db_ssh: SSHConection;
     private db_config: DbConfig
     constructor() {
         this.conection = mysql;
+        this.db_ssh = new SSHConection();
         this.db_config = new DbConfig();
     }
     get normal_connection(): Connection {
@@ -12,5 +15,14 @@ export class DbConection {
     }
     get pool_connection(): Pool {
         return this.conection.createPool(this.db_config.pool_config)
+    }
+
+    async ssh_conection(): Promise<Connection>{
+        const ssh = this.db_ssh
+        return new Promise((resolve, reject)=>{
+            const conn = ssh.ssh_conection()
+            conn.then((result) => resolve(result))
+            conn.catch((err)=>reject(err))
+        })
     }
 }
