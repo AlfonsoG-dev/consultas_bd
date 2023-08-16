7829bc1ac718b55b7f74d8c481d3be5197662a7a
tuNovio <alfgomachi@gmail.com>
2023-08-16 10:51:08 -0500

    las rutas cambian para hacer match de tipos

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -19,10 +19,10 @@ export class UserRoutes implements RoutesModel{
     get_all_route(){
         this.user_routes.get("/", this.controller.read_all.bind(this.controller))
     }
-    get_by_name_route(): void {
+    get_name_route(): void {
         this.user_routes.post("/get-name", this.controller.read_by_name.bind(this.controller))
     }
-    get_by_email_route(): void {
+    get_email_route(): void {
         this.user_routes.post("/get-email", this.controller.read_by_email.bind(this.controller))
     }
     get_by_id_route(): void {
