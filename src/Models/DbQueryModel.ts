7c1d5ec71c6d9813ac967c73554c1621038d141d
tuNovio <alfgomachi@gmail.com>
2023-08-16 11:49:43 -0500

    acepta 1 o muchos argumentos depdende de la implementación

--- src/Models/DbQueryModel.ts
+++ src/Models/DbQueryModel.ts
@@ -5,10 +5,10 @@ export interface DbQueryModel{
     verificate_table(): Promise<ResultSetHeader | undefined>
     select_database(): Promise<ResultSetHeader | undefined>
     get read_all(): Promise<RowDataPacket[] | undefined>
-    read_by_email(T: any): Promise<RowDataPacket[] | undefined>
-    read_by_name(T: any): Promise<RowDataPacket[] | undefined>
-    read_by_id(T: any):Promise<RowDataPacket[] | undefined>
-    insert_register(T: any):Promise<ResultSetHeader | undefined>
-    update_register(T: any):Promise<ResultSetHeader | undefined>
-    delete_register(T: any):Promise<ResultSetHeader | undefined>
+    read_by_email(...T: any): Promise<RowDataPacket[] | undefined>
+    read_by_name(...T: any): Promise<RowDataPacket[] | undefined>
+    read_by_id(...T: any):Promise<RowDataPacket[] | undefined>
+    insert_register(...T: any):Promise<ResultSetHeader | undefined>
+    update_register(...T: any):Promise<ResultSetHeader | undefined>
+    delete_register(...T: any):Promise<ResultSetHeader | undefined>
 }
