5809c5b33463f1cf2e9177faba3d83a46528aa09
tuNovio <alfgomachi@gmail.com>
2023-08-18 16:09:23 -0500

    refactor: solución al problema de muchas conexiones

--- src/controllers/DbController.ts
+++ src/controllers/DbController.ts
@@ -1,17 +1,12 @@
 import {UserTypes} from "../Models/User";
 import { DbConection } from "../services/DbConection";
-
+const conection = new DbConection().normal_connection
 //tipo de dato para la consulta
 type T= any
-export class DbController extends DbConection{
-    private cursor;
-    constructor(){
-        super();
-        this.cursor = super.pool_connection();
-    }
+export class DbController{
     any_execute(sql: string, ...args: any): Promise<T>{
         return new Promise((resolve, reject)=>{
-            this.cursor.execute(sql, args, function(err, res: UserTypes[]){
+            conection.execute(sql, args, function(err, res: UserTypes[]){
                 if(err) reject(err)
                 resolve(res)
             })
@@ -19,7 +14,7 @@ export class DbController extends DbConection{
     }
     any_query(sql: string, ...args: any): Promise<T>{
         return new Promise((resolve, reject)=>{
-            this.cursor.query(sql, args, function(err, res){
+            conection.query(sql, args, function(err, res){
                 if(err) reject(err)
                 resolve(res)
             })
